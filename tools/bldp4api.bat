2262b61fd1886fea1d4c1039c724e7ada69fa59f
ajindal-mdx <87968284+ajindal-mdx@users.noreply.github.com>
2025-10-16 16:32:58 +0530

    Update bldp4api.bat

--- tools/bldp4api.bat
+++ tools/bldp4api.bat
@@ -1,9 +1,7 @@
 :: Build P4 API.
 :: Run from P4 folder.
-::jam -s OSPLAT=X64 -s SSL=yes -s LDAPSTUB=no -s TYPE=dyn -s SMARTHEAP=no -s USE_EXTENSIONS=no -s API=x
+:: jam -s OSPLAT=X64 -s SSL=yes -s LDAPSTUB=no -s TYPE=dyn -s SMARTHEAP=no -s USE_EXTENSIONS=no -s API=x
 
-:: New settings based on email from Brett Taylor 2021/12/17 to Kevin Mackey (and copying Joel Kovisto).
-:: TYPE=dyn was added to match above.
 if "%MSVSVER%" == "15" goto next1
 set C++FLAGS=-DHAS_CPP11
 :next1
